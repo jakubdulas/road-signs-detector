5b0b810e98d58963d08ccb24f5c21d35aba98d33
Jakub Dulas <jakubdulas.mail@gmail.com>
2023-05-07 21:49:01 +0200

    adding confidance rate and own borders

--- RoadSignsDetector.py
+++ RoadSignsDetector.py
@@ -4,41 +4,56 @@ import numpy as np
 
 
 class RoadSignsDetector():
-    class_names = ['c4', 'c2', 'b20', 'c12', 'a7']
+    CLASS_NAMES = ['c4', 'c2', 'b20', 'c12']
+    COLORS = [(255, 0, 0), (0, 255, 255), (0, 255, 0), (0, 0, 255)]
 
-    def __init__(self) -> None:
+    def __init__(self, confidence_rate=0.5) -> None:
         self.model = torch.hub.load('ultralytics/yolov5', 'custom', path='weights/20e_street_20e_printed.pt')
         self.predicted_signs = []
+        self.confidence_rate = confidence_rate
 
     def predict(self, frame):
-        original_size = frame.shape[:2][::-1]
+        # original_size = frame.shape[:2][::-1]
         img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        img = cv2.resize(img, (640, 640))
+        # img = cv2.resize(img, (640, 640))
 
         result = self.model(img)
 
-        rendered_img = result.render()[0]
-        rendered_img = cv2.resize(rendered_img, original_size)
-        rendered_img = cv2.cvtColor(rendered_img, cv2.COLOR_RGB2BGR)
+
+        # rendered_img = result.render()[0]
+        # rendered_img = cv2.resize(rendered_img, original_size)
+        # rendered_img = cv2.cvtColor(rendered_img, cv2.COLOR_RGB2BGR)
 
         self.predicted_signs = []
         for pred in result.xyxyn[0]:
-            class_name = self.class_names[int(pred[5])]
-            self.predicted_signs.append((*pred[:4].numpy(), class_name))
+            if self.confidence_rate > float(pred[4]):
+                continue
 
-        return rendered_img
+            class_name = self.CLASS_NAMES[int(pred[5])]
+            self.predicted_signs.append((*pred[:4].numpy(), class_name))
+        
+        for x1, y1, x2, y2, class_name in self.predicted_signs:
+            x1 = int(x1*img.shape[1])
+            y1 = int(y1*img.shape[0])
+            x2 = int(x2*img.shape[1])
+            y2 = int(y2*img.shape[0])
+            color = self.COLORS[self.CLASS_NAMES.index(class_name)]
+            img = cv2.rectangle(img, (x1, y1), (x2, y2), color, 5)
+            img = cv2.putText(img,  class_name, (x1, y1), 1, 3, color, 5)
+
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+        return img
     
     def get_predicted_signs(self):
         """
-        The function returns a list of detected road signs as: (x_cetner, y_center, width, height, class_name).
-        The coordintates are represented in the form of floats which are relative distances from left top corner.
+        The function returns a list of detected road signs as: (x1, y1, x2, y2, class_name).
+        The coordintates are represented in the form of floats which are relative distances from left top corner of the screen.
         """
         return self.predicted_signs
 
 if __name__ == '__main__':
-    print("Hello world")
-
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(1)
     detector = RoadSignsDetector()
 
     if not cap.isOpened():
@@ -53,7 +68,8 @@ if __name__ == '__main__':
         img = detector.predict(frame)
         predicted_signs = detector.get_predicted_signs()
 
-        print(predicted_signs)
+        if len(predicted_signs):
+            cv2.putText(img, f"Znak {predicted_signs[0][-1]}", (50, 75), 1, 5, (0, 0, 255), 2)
 
         cv2.imshow('frame', img)
         if cv2.waitKey(1) == ord('q'):
